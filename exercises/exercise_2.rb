85a935c832ccc2431b0bc60db634fc664c84b882
Liam Smith <liam.s.smith@gmail.com>
2022-07-01 03:01:51 +0000

    complete exercises 1 and 2

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -4,4 +4,9 @@ require_relative './exercise_1'
 puts "Exercise 2"
 puts "----------"
 
-# Your code goes here ...
+store1 = Store.first
+store2 = Store.second
+puts store1.name
+puts store2.name
+store1.update(annual_revenue: 325000)
+puts " The #{store1.name} store's annual revenue is $#{store1.annual_revenue}"
