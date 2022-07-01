104269b09ad938dd80ec2f1186614c9262239010
Liam Smith <liam.s.smith@gmail.com>
2022-07-01 17:24:51 +0000

    complete exercise 5, average revenue and high grossing stores

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -7,4 +7,11 @@ require_relative './exercise_4'
 puts "Exercise 5"
 puts "----------"
 
-# Your code goes here ...
+@total_revenue = Store.sum('annual_revenue')
+puts @total_revenue
+
+@average_revenue = @total_revenue/Store.count
+puts @average_revenue
+
+@high_grossing_stores = Store.where("annual_revenue >= ?", 1000000).count
+puts @high_grossing_stores
