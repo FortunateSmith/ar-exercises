e62fc3c580363142fcc9eebd6c2985d4dce3c8cb
Liam Smith <liam.s.smith@gmail.com>
2022-07-02 02:31:58 +0000

    update variables to instance variables

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -4,9 +4,9 @@ require_relative './exercise_1'
 puts "Exercise 2"
 puts "----------"
 
-store1 = Store.first
-store2 = Store.second
-puts store1.name
-puts store2.name
-store1.update(annual_revenue: 325000)
-puts " The #{store1.name} store's annual revenue is $#{store1.annual_revenue}"
+@store1 = Store.first
+@store2 = Store.second
+puts @store1.name
+puts @store2.name
+@store1.update(annual_revenue: 325000)
+puts " The #{@store1.name} store's annual revenue is $#{@store1.annual_revenue}"
