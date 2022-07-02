a4b9365b84a7d3cc32efacf8a4d8ab69a1aea9b1
Liam Smith <liam.s.smith@gmail.com>
2022-07-02 02:33:13 +0000

    complete exercise 6, add employees to table and set relation of Store and Employee models

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -8,4 +8,14 @@ require_relative './exercise_5'
 puts "Exercise 6"
 puts "----------"
 
-# Your code goes here ...
+
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Cillian", last_name: "Smith", hourly_rate: 125)
+@store2.employees.create(first_name: "Vincent", last_name: "Smith", hourly_rate: 125)
+@store1.employees.create(first_name: "Val", last_name: "Richards", hourly_rate: 55)
+@store1.employees.create(first_name: "Mike", last_name: "Zorn", hourly_rate: 45)
+@store2.employees.create(first_name: "Chad", last_name: "Nunn", hourly_rate: 55)
+@store2.employees.create(first_name: "Jay", last_name: "Dorsey", hourly_rate: 39)
+
+@employee = Employee.where(store: 2).count
+puts @employee
